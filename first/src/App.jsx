386f33d35d2041c7cdf0b6e7fd692e0bc6ca18ed
mohitpal55 <rs.mohit8304@gmail.com>
2026-02-24 07:34:59 +0000

    name

---
+++ first/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1>Mohit PAL </h1>
+    <h1>hello</h1>
+    <Emp></Emp>
+ </div> )
+}
+
+function Emp (){
+  return(
+    <h2>MOHIT</h2>
+  )
+}
+
+export default App
