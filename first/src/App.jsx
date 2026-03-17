1ebeae97090aef89f5a67b54f9792fdab8fbb933
mohitpal55 <rs.mohit8304@gmail.com>
2026-03-17 07:37:34 +0000

    fourth lab

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -5,26 +5,32 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const name ="abhi";
+  let x=24;
+  let y=29;
+  function fun()
+  {
+    return"inside fun function";
+  }
+  function sum(x,y)
+  {
+    return x+y;
+  }
+  
 
-  return (
+   return (
+
     <div>
-    <h1>Mohit PAL </h1>
-    <h1>hello</h1>
-    <Emp></Emp>
-    <Branch></Branch>
- </div> )
+    <h1>project run</h1>
+  <h3>value of variable is {name}</h3>
+  <h3>{x+y}</h3>
+  <h1>use of functuons with jsx</h1>
+  <h1>MOHIT</h1>
+  <h3>{fun()}</h3>
+  <h3>{sum (20,8)}</h3>
+ </div> 
+ )
 }
 
-function Emp (){
-  return(
-    <h2>MOHIT</h2>
-  )
-}
-function Branch()
-{
-  return( 
-    <h3>10/03</h3>
-  )
 
-}
 export default App
--- first/src/main.jsx
+++ first/src/main.jsx
@@ -7,6 +7,6 @@ import Demo from './demo/ten.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Demo /> 
+    
   </StrictMode>,
 )
