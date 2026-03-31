86ea85b2175bb1a9199f69dc5981fba90b245384
mohitpal55 <rs.mohit8304@gmail.com>
2026-03-31 07:34:01 +0000

    5 lab

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -5,32 +5,27 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const name ="abhi";
-  let x=24;
-  let y=29;
-  function fun()
-  {
-    return"inside fun function";
-  }
-  function sum(x,y)
-  {
-    return x+y;
-  }
-  
-
-   return (
-
-    <div>
-    <h1>project run</h1>
-  <h3>value of variable is {name}</h3>
-  <h3>{x+y}</h3>
-  <h1>use of functuons with jsx</h1>
-  <h1>MOHIT</h1>
-  <h3>{fun()}</h3>
-  <h3>{sum (20,8)}</h3>
- </div> 
- )
+   
+            function callfun()
+            {
+                alert ("Function call");
+    
+            }
+            function apple()
+            {
+              alert ("Function apple");
+            }
+            function banana()
+            {
+              alert("Function banana");
+            }
+            
+             return (
+              <div>
+              <button onClick={callfun}>OK</button>
+              <button onClick={apple}>apple</button>
+              <button onClick={banana}>banana</button>        
+              </div>
+   )
 }
-
-
 export default App
